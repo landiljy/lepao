63aa847a6cf89f3224d7a0670e87b1f8b414d9ac
ljy <qindiljy@qq.com>
2017-05-19 20:18:27 +0800

    母版程序cs

--- app/src/main/java/com/gxu/lepao/PlayingActivity.java
+++ app/src/main/java/com/gxu/lepao/PlayingActivity.java
@@ -10,7 +10,7 @@ public class PlayingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playing);
+        setContentView(R.layout.activity_login);
 
     }
 
